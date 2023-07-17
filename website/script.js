da585dc04b837c8b1755efc66c4aace74113e413
kaustubhchaudhari05 <kaustubh.d.chaudhari38@gmail.com>
2023-07-17 15:43:48 +0530

    git pull

--- website/script.js
+++ website/script.js
@@ -8,3 +8,7 @@ let subProject = document.getElementById("subProject");
 function toggleProject() {
     subProject.classList.toggle("open-project");
 }
+let subFolder = document.getElementById("subFolder");
+function toggleFolder() {
+    subFolder.classList.toggle("open-folder");
+}
